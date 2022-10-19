3af1f2da1885b6bf070c1342ec317a55816a0554
JenniferVR2022 <jennifer.velasquez0@misena.edu.co>
2022-10-19 07:27:34 -0500

    errror-usuario

--- usuarios/views.py
+++ usuarios/views.py
@@ -31,20 +31,3 @@ def usuarios_crear(request):
     }
     return render(request,'usuarios/usuarios-crear.html',context)
 
-def usuarios_editar(request, pk):
-    titulo="Usuarios - Editar"
-    usuario= Usuario.objects.get(id=pk)
-    if request.method == "POST":
-        form= UsuarioForm(request.POST, instance=usuario)
-        if form.is_valid():
-            form.save()
-            return redirect('usuarios')
-        else:
-            print("Error al guardar")
-    else:
-        form= UsuarioForm(instance=usuario)
-    context={
-        'titulo':titulo,
-        'form':form
-    }
-    return render(request,'usuarios/usuarios-crear.html',context)
