7cbb42372106a6738266320cf78c9bf747f1f448
JenniferVR2022 <jennifer.velasquez0@misena.edu.co>
2022-12-12 04:59:54 -0500

    cambios ultimos

--- base/settings.py
+++ base/settings.py
@@ -99,22 +99,6 @@ DATABASES = {
 }
 
 
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    },
-    'select2': {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
--- receta/forms.py
+++ receta/forms.py
@@ -1,6 +1,5 @@
 from django.forms import ModelForm, widgets
 from django_select2 import forms as s2forms
-from ingrediente.models import Ingrediente
 from receta.models import Receta,RecetaDetalle
 
 
