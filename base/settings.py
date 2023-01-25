9dfb597e20622455db0279ffc39ab6e404a114a4
Kroonozz <David.s.1491@gmail.com>
2023-01-25 14:34:28 -0500

    Cambios 25 Enero

--- base/settings.py
+++ base/settings.py
@@ -94,9 +94,14 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.mysql',
+        'USER': 'root',
+        'NAME': 'indualiapp',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
+     }
 }
 
 
