01f72cde71fd9606bd2346bfcca13c1dc053b608
Kroonozz <David.s.1491@gmail.com>
2023-01-26 10:06:00 -0500

    Cambios David

--- ingrediente/models.py
+++ ingrediente/models.py
@@ -4,8 +4,12 @@ from unittest.util import _MAX_LENGTH
 # Create your models here.
 
 class Ingrediente (models.Model):
-    codIngrediente=models.CharField(max_length=50, verbose_name="Cod Ingrediente")
-    nomIngrediente=models.CharField(max_length=50, verbose_name="Nom Ingrediente")
-    unidadMedida=models.CharField(max_length=50, verbose_name="Unidad Medida")
+    codIngrediente=models.CharField(max_length=50, verbose_name="Codigo Ingrediente")
+    nomIngrediente=models.CharField(max_length=50, verbose_name="Nombre Ingrediente")
+    unidadMedida=models.CharField(max_length=50, verbose_name="Unidad de Medida")
     cantidadMateriaPrima=models.CharField(max_length=50, verbose_name="Cantidad Materia Prima")
     
+
+    def __str__(self):
+        fila = "Nombre: "+ self.nomIngrediente + " - Unidad: " + self.unidadMedida 
+        return fila
