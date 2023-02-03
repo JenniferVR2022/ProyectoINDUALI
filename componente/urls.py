e979b3de16dca698902c2389586c37e41dac0155
JenniferVR2022 <jennifer.velasquez0@misena.edu.co>
2023-02-03 07:37:06 -0500

    02 feb 2023

--- componente/urls.py
+++ componente/urls.py
@@ -1,8 +1,8 @@
 
 from django.urls import path
-from componente.views import componente
+from componente.views import componente,crear_componente
 
 urlpatterns = [
     path('componente/',componente,name="componente"),
-
+    path('crear/',crear_componente,name="crear-componente"),
 ]
