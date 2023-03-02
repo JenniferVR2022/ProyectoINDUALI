8adb837d519171a71d22ae1bcc6e8c6ce71a7ff2
IvanBoyacen <112267039+IvanBoyacen@users.noreply.github.com>
2023-03-01 21:01:16 -0500

    CAMBIOS 01/03

--- ingrediente/models.py
+++ ingrediente/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from unittest.util import _MAX_LENGTH
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
@@ -7,7 +8,7 @@ class Ingrediente (models.Model):
     codIngrediente=models.CharField(max_length=50, verbose_name="Codigo Ingrediente")
     nomIngrediente=models.CharField(max_length=50, verbose_name="Nombre Ingrediente")
     unidadMedida=models.CharField(max_length=50, verbose_name="Unidad de Medida")
-    cantidadMateriaPrima=models.CharField(max_length=50, verbose_name="Cantidad Materia Prima")
+    costoIngrediente=models.CharField(max_length=50, verbose_name="Costo Ingrediente")
     estado = models.BooleanField (default=True,verbose_name="Estado")
 
     def __str__(self):
