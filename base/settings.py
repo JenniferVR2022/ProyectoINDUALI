48ba6ad11eab9bf082ec85c5715cab2cfa66a0e5
David Escudero Cortés <David.s.1491@gmail.com>
2023-05-29 21:56:17 -0500

    Cambio de Base de Datos

--- base/settings.py
+++ base/settings.py
@@ -96,15 +96,10 @@ WSGI_APPLICATION = 'base.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.mysql',
-        'NAME':     'induali2023',
-        'USER':     'induali2023',
-        'PASSWORD': 'Induali2023',
-        'HOST':     'db4free.net',
-        'PORT': 3306,
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
