8634e14d71d4f38972ceea50f1d59097aed76fd1
JenniferVR2022 <jennifer.velasquez0@misena.edu.co>
2023-06-15 21:59:41 -0500

    JENNIFER 15-06-2023

--- centroCostos/admin.py
+++ centroCostos/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import CentroCosto
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 # Register your models here.
+
+class CentroCostoResource(resources.ModelResource):
+    class Meta:
+        model = CentroCosto
+
+class CentroCostoAdmin(ImportExportModelAdmin, admin.ModelAdmin):
+    search_fields = ['nomCentroCostos']
+    list_display = ('codCentroCostos', 'nomCentroCostos',)
+    resource_class = CentroCostoResource
+    
+admin.site.register(CentroCosto, CentroCostoAdmin)
