1278dbb815754d73692419dd79d228dec98dcfba
JenniferVR2022 <110873245+JenniferVR2022@users.noreply.github.com>
2023-06-21 20:58:06 -0500

    JENNIFER 21-06-2023

--- receta/forms.py
+++ receta/forms.py
@@ -3,7 +3,7 @@ from .models import Receta, Ingrediente
 
 class recetaForm(forms.ModelForm):
     cantidadMateriaPrima = forms.DecimalField(label="Cantidad de Materia Prima", decimal_places=2, min_value=0)
-    nomIngrediente = forms.ModelMultipleChoiceField(
+    ingredientes = forms.ModelMultipleChoiceField(
         queryset=Ingrediente.objects.all(),
         widget=forms.CheckboxSelectMultiple,
         required=False  # Permite que el campo sea opcional
@@ -11,20 +11,20 @@ class recetaForm(forms.ModelForm):
 
     class Meta:
         model = Receta
-        fields = ['nomComponente', 'codReceta', 'nomReceta', 'estado', 'estandar', 'preparacion', 'cantidadMateriaPrima', 'nomIngrediente']
+        fields = ['nomComponente', 'codReceta', 'nomReceta', 'estado', 'estandar', 'preparacion', 'cantidadMateriaPrima', 'ingredientes']
 
     def save(self, commit=True):
         receta = super().save(commit=False)
         
         # Limpiar la relación ManyToMany existente
-        receta.nomIngrediente.clear()
+        receta.ingredientes.clear()
 
         # Guardar la receta para obtener un ID válido
         if commit:
             receta.save()
 
         # Agregar los ingredientes seleccionados a la receta
-        for ingrediente in self.cleaned_data['nomIngrediente']:
-            receta.nomIngrediente.add(ingrediente)
+        for ingrediente in self.cleaned_data['ingredientes']:
+            receta.ingredientes.add(ingrediente)
 
         return receta
